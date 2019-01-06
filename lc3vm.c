97e3e96b5d973fc005f80e55f570d199ef989d06
Mark Wissink <markcwissink@gmail.com>
2019-01-05 22:18:50 -0500

    Finished implementing opcodes

--- lc3vm.c
+++ lc3vm.c
@@ -44,6 +44,17 @@ enum {
     FL_NEG = 1 << 2, /* N */
 };
 
+// Trap codes
+enum
+{
+    TRAP_GETC = 0x20,  /* get character from keyboard */
+    TRAP_OUT = 0x21,   /* output a character */
+    TRAP_PUTS = 0x22,  /* output a word string */
+    TRAP_IN = 0x23,    /* input a string */
+    TRAP_PUTSP = 0x24, /* output a byte string */
+    TRAP_HALT = 0x25   /* halt the program */
+};
+
 // Updates the condition flag register
 void update_flags(uint16_t r)
 {
@@ -211,24 +222,102 @@ int main(int argc, const char* argv[])
                 }
                 break;
             case OP_LEA:
-                {LEA, 7}
+                {
+                    uint16_t dr = (instr >> 9) & 0x7;
+                    uint16_t pc_offset = sign_extend(instr & 0x1ff, 9);
+                    reg[dr] = reg[R_PC] + pc_offset;
+                    update_flags(dr);
+                }
                 break;
             case OP_ST:
-                {ST, 7}
+                {
+                    uint16_t sr = (instr >> 9) & 0x7;
+                    uint16_t pc_offset = sign_extend(instr & 0x1ff, 9);
+                    mem_write(reg[R_PC] + pc_offset, reg[sr]);
+                }
                 break;
             case OP_STI:
-                {STI, 7}
+                {
+                    uint16_t sr = (instr >> 9) & 0x7;
+                    uint16_t pc_offset = sign_extend(instr & 0x1ff, 9);
+                    mem_write(mem_read(reg[R_PC] + pc_offset), reg[sr]);
+                }
                 break;
             case OP_STR:
-                {STR, 7}
+                {
+                    uint16_t sr = (instr >> 9) & 0x7;
+                    uint16_t br = (instr >> 6) & 0x7;
+                    uint16_t offset = sign_extend(instr & 0x3F, 6);
+                    mem_write(reg[br] + offset, reg[sr]);
+                }
                 break;
             case OP_TRAP:
-                {TRAP, 8}
+                {
+                    switch (instr & 0xFF)
+                    {
+                      case TRAP_GETC:
+                          {
+                              /* read a single ASCII char */
+                              reg[R_R0] = (uint16_t)getchar();
+                          }
+                          break;
+                      case TRAP_OUT:
+                          {
+                              putc((char)reg[R_R0], stdout);
+                              fflush(stdout);
+                          }
+                          break;
+                      case TRAP_PUTS:
+                          {
+                              /* one char per word */
+                              uint16_t* c = memory + reg[R_R0];
+                              while (*c)
+                              {
+                                  putc((char)*c, stdout);
+                                  ++c;
+                              }
+                              fflush(stdout);
+                          }
+                          break;
+                      case TRAP_IN:
+                          {
+                              printf("Enter a character: ");
+                              reg[R_R0] = (uint16_t)getchar();
+                          }
+                          break;
+                      case TRAP_PUTSP:
+                          {
+                            /* one char per byte (two bytes per word)
+                               here we need to swap back to
+                               big endian format */
+                            uint16_t* c = memory + reg[R_R0];
+                            while (*c)
+                            {
+                                char char1 = (*c) & 0xFF;
+                                putc(char1, stdout);
+                                char char2 = (*c) >> 8;
+                                if (char2) putc(char2, stdout);
+                                ++c;
+                            }
+                            fflush(stdout);
+                          }
+                          break;
+                      case TRAP_HALT:
+                          {
+                              puts("HALT");
+                              fflush(stdout);
+                              running = 0;
+                          }
+                          break;
+                    }
+                }
                 break;
             case OP_RES:
             case OP_RTI:
             default:
-                {BAD OPCODE, 7}
+                {
+                  abort();
+                }
                 break;
         }
     }
